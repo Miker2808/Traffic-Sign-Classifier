37c9a070037099d86af09b24d7cb3b39460af17c
Michael Pogodin <miker2808@gmail.com>
2024-07-29 23:14:59 +0300

    Changed model, increased accuracy, and added save and weight load functions

--- traffic_sign_classifier.py
+++ traffic_sign_classifier.py
@@ -7,71 +7,84 @@ from torchvision import datasets, transforms
 
 class TrafficSignCNN(nn.Module):
     
-    def __init__(self, gpu=False):
+    def __init__(self, device="cpu"):
         super().__init__()
-
-        self.gpu = gpu
-
+        self.device = device
+        self.num_classes = 43
         # Convolutional layers
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3)
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3)
-        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3)
        
         # pool layers
-        self.pool = nn.MaxPool2d(kernel_size=2,stride=2)
+        self.pool = nn.MaxPool2d(kernel_size=2,stride=2, padding=0)
         
         # dropout layers
         self.dropout = nn.Dropout(0.5)
 
         # Fully connected layer
-        self.fullc1 = nn.Linear(in_features=1600, out_features= 800)
-        self.fullc2 = nn.Linear(in_features=800, out_features= 200)
-        self.fullc3 = nn.Linear(in_features=200, out_features= 43)
+        self.fullc1 = nn.Linear(in_features=512, out_features= 256)
+        self.fullc2 = nn.Linear(in_features=256, out_features= 128)
+        self.fullc3 = nn.Linear(in_features=128, out_features= self.num_classes)
+
+    def save_model(self, name="classifier_weights"):
+        torch.save(self.state_dict(), f"weights/{name}.pt")
+
+    def load_model(self, path):
+        self.load_state_dict(torch.load(path))
 
     def forward(self, x):
         # Convolution and pooling layers
         x = F.relu(self.conv1(x))
         x = self.pool(x)
         x = F.relu(self.conv2(x))
+        x = self.pool(x)
         x = F.relu(self.conv3(x))
         x = self.pool(x)
 
-        x = x.view(-1, 1600) # flatten first fully connected layer input
+        x = x.view(-1, 512) # flatten first fully connected layer input
 
         # Fully connected layers
         x = F.relu(self.fullc1(x))
         x = self.dropout(x)
         x = F.relu(self.fullc2(x))
         x = self.dropout(x)
-        x = F.log_softmax(self.fullc3(x), dim=1)
+        x = self.fullc3(x)
 
         return x
     
     # evaluate the models performance on the test set, do not update the weights on the new data
     def __test_batches(self, test_loader : DataLoader, criterion : nn.CrossEntropyLoss, optimizer : torch.optim.Adam):
         print("Testing model")
-        
         tests_correct = 0
-        tests_count = 0
+        tests_total = 0
         with torch.no_grad():
             for batch, (x_test, y_test) in enumerate(test_loader):
                 batch += 1
-                tests_count += 1
 
-                y_val = self.forward(x_test)
-                predicted = torch.max(y_val.data, 1)[1]
-                tests_correct = (predicted == y_test).sum()
+                x_test, y_test = x_test.to(self.device), y_test.to(self.device)
+
+                outputs = self.forward(x_test)
+                _, predicted = torch.max(outputs.data, 1)
+
+                tests_total += y_test.size(0)
+                
+                tests_correct += (predicted == y_test).sum().item()
+
                 if batch % 100 == 0:
                     print(f"Test Batch {batch}")
         
-        loss = criterion(y_val, y_test)
-        print(f"Tests correct: {tests_correct} / {tests_count}, loss: {loss}")
+        loss = criterion(outputs, y_test)
+        print(f"Tests correct: {tests_correct} / {tests_total}, loss: {loss}")
 
 
     def __train_batches(self, train_loader : DataLoader, criterion: nn.CrossEntropyLoss, optimizer : torch.optim.Adam):
         print("Training model")
         
         for batch, (x_train, y_train) in enumerate(train_loader):
+
+            x_train, y_train = x_train.to(self.device), y_train.to(self.device)
+
             batch += 1
              
             # predict
@@ -91,24 +104,25 @@ class TrafficSignCNN(nn.Module):
     # Train the model on train dataset
     # learning_rate is the gradient step, and decreasing it decreases chances for overstepping, 
     # but takes significantly longer to optimize (train)
-    def train_model(self, train_loader : DataLoader, test_loader : DataLoader, epochs=5, learning_rate=0.001):
+    def train_model(self, train_loader : DataLoader, test_loader : DataLoader, epochs=10, learning_rate=0.001):
         # Loss function
         criterion = nn.CrossEntropyLoss()
         # Optimizer
-        optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
+        optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate, weight_decay=0.001)
 
-        for i in range(epochs):
+        for i in range(1, epochs+1):
             print(f"Epoch {i}")
             self.__train_batches(train_loader, criterion, optimizer)
             self.__test_batches(test_loader, criterion, optimizer)
-        
-
-    
     
 
 def main():
     # Transformer to change dataset into tensors
-    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    if torch.cuda.is_available():
+        print(f"GPU available, running on: {torch.cuda.get_device_name(device)}")
+
     transform = transforms.Compose([
                 transforms.Resize((32, 32)),  # Resize images to 32x32
                 transforms.ToTensor(),        # Convert images to PyTorch tensors
@@ -121,12 +135,13 @@ def main():
     test_data = datasets.GTSRB(root="gtsrb/test", split="test",
                                 download=True, transform=transform)
 
-    # Create batch sizes for the data
-    train_loader = DataLoader(train_data, batch_size=10, shuffle=True)
-    test_loader = DataLoader(test_data, batch_size=10, shuffle=False)
+    # Create batch sizes for the data, support increased number of workers (multiprocessing), and moving tasks to GPU
+    train_loader = DataLoader(train_data, batch_size=5, shuffle=True, num_workers=6, pin_memory=True)
+    test_loader = DataLoader(test_data, batch_size=5, shuffle=False, num_workers=6, pin_memory=True)
 
-    model = TrafficSignCNN()
+    model = TrafficSignCNN(device).to(device)
     model.train_model(train_loader, test_loader)
+    model.save_model()
 
 
 if __name__ == "__main__":
